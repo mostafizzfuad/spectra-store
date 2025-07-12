12f50d779ed8b4594b148ed0ffdf65cecad82ba7
Mostafizur <mostafizzfuad@gmail.com>
2025-07-13 01:37:11 +0600

    header cart button, sign-in button and theme mode toggle added

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,19 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
-// import Menu from "./menu";
-// import CategoryDrawer from "./category-drawer";
-// import Search from "./search";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/shared/header/mode-toggle";
 
 const Header = () => {
 	return (
 		<header className="w-full border-b">
 			<div className="wrapper flex-between">
 				<div className="flex-start">
-					{/* <CategoryDrawer /> */}
 					<Link href="/" className="flex-start ml-4">
 						<Image
-							src="/images/logo.svg"
+							src="./images/logo.svg"
 							alt={`${APP_NAME} logo`}
 							height={48}
 							width={48}
@@ -24,8 +23,19 @@ const Header = () => {
 						</span>
 					</Link>
 				</div>
-				<div className="hidden md:block">{/* <Search /> */}</div>
-				{/* <Menu /> */}
+				<div className="space-x-2">
+					<ModeToggle />
+					<Button asChild variant="ghost">
+						<Link href="/cart">
+							<ShoppingCart /> Cart
+						</Link>
+					</Button>
+					<Button asChild variant="ghost">
+						<Link href="/sign-in">
+							<UserIcon /> Sign In
+						</Link>
+					</Button>
+				</div>
 			</div>
 		</header>
 	);
