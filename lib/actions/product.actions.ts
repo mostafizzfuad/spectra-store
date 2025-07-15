caa6d33dbb1ec16edd40b2cbadd256d23ff05294
Mostafizur <mostafizzfuad@gmail.com>
2025-07-15 19:44:25 +0600

    Config Servlerless Environment

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,12 @@
 "use server";
-import { PrismaClient } from "../generated/prisma";
+// import { PrismaClient } from "../generated/prisma";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-	const prisma = new PrismaClient();
+	// const prisma = new PrismaClient();
 
 	const data = await prisma.product.findMany({
 		take: LATEST_PRODUCTS_LIMIT,
