e66fa258a35008a38be524bcd9cd34b602c7dbf9
Mostafizur <mostafizzfuad@gmail.com>
2025-07-17 02:19:18 +0600

    Added UserButton and Sign-Out

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,6 +8,6 @@ export const LATEST_PRODUCTS_LIMIT =
 	Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-	email: "admin@example.com",
-	password: "123456",
+	email: "",
+	password: "",
 };
