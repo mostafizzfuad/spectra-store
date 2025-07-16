8f654832987f0ff537b32b37f9ba48bfed3fc051
Mostafizur <mostafizzfuad@gmail.com>
2025-07-16 18:13:44 +0600

    Seed User Data

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from "bcrypt-ts-edge";
+
 const sampleData = {
+	users: [
+		{
+			name: "Mostafizur",
+			email: "admin@example.com",
+			password: hashSync("123456", 10),
+			role: "admin",
+		},
+		{
+			name: "Rifat",
+			email: "user@example.com",
+			password: hashSync("123456", 10),
+			role: "user",
+		},
+	],
 	products: [
 		{
 			name: "Polo Sporting Stretch Shirt",
