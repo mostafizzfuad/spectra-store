5153a2725b882fda6932d809fd4217ae18a56bd7
Mostafizur <mostafizzfuad@gmail.com>
2025-07-19 21:58:05 +0600

    Calculate price and Add to DB, Remove Cart action and so on

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
 import AddToCart from "@/components/shared/product/add-to-cart";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 const ProductDetailsPage = async (props: {
 	params: Promise<{ slug: string }>;
@@ -12,6 +13,8 @@ const ProductDetailsPage = async (props: {
 	const { slug } = await props.params;
 	const product = await getProductBySlug(slug);
 	if (!product) notFound();
+
+	const cart = await getMyCart();
 
 	return (
 		<>
@@ -67,7 +70,9 @@ const ProductDetailsPage = async (props: {
 								</div>
 								{product.stock > 0 && (
 									<div className="flex-center">
-										<AddToCart item={{ 
+										<AddToCart
+										cart={cart}
+										item={{ 
 											productId: product.id,
 											name: product.name,
 											slug: product.slug,
--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import NextAuth from "next-auth";
+import { cookies } from "next/headers";
 
 const config = {
 	pages: {
@@ -81,6 +82,31 @@ const config = {
 						where: { id: user.id },
 						data: { name: token.name },
 					});
+				}
+
+				if (trigger === "signIn" || trigger === "signUp") {
+					const cookiesObject = await cookies();
+					const sessionCartId =
+						cookiesObject.get("sessionCartId")?.value;
+
+					if (sessionCartId) {
+						const sessionCart = await prisma.cart.findFirst({
+							where: { sessionCartId },
+						});
+
+						if (sessionCart) {
+							// Delete current user cart
+							await prisma.cart.deleteMany({
+								where: { userId: user.id },
+							});
+
+							// Assign new cart
+							await prisma.cart.update({
+								where: { id: sessionCart.id },
+								data: { userId: user.id },
+							});
+						}
+					}
 				}
 			}
 
