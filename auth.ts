eda4b94417c3b23a9afaa465bdeebfa028a8924a
Mostafizur <mostafizzfuad@gmail.com>
2025-07-19 22:55:39 +0600

    Refactor auth middleware to move authorization callback & config logic to seperate file to reduce size of Edge Function code that Vercel processes

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
+import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
-import NextAuth from "next-auth";
 import { cookies } from "next/headers";
+import { authConfig } from "./auth.config";
 
 const config = {
 	pages: {
@@ -117,6 +118,7 @@ const config = {
 
 			return token;
 		},
+		...authConfig.callbacks,
 	},
 };
 
