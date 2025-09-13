fde1e7d3ec3c6f0e3294242bf2b3bd6bc391ace5
Mostafizur <mostafizzfuad@gmail.com>
2025-09-13 19:07:29 +0600

    Added Admin Orders Page

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -29,7 +29,9 @@ const OrderDetailsPage = async (props: {
 				...order,
 				shippingAddress: order.shippingAddress as ShippingAddress,
 			}}
+			stripeClientSecret={client_secret}
 			paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+			isAdmin={session?.user?.role === "admin" || false}
 		/>
 	);
 };
