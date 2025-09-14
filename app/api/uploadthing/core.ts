486d068fd392dee3221100f53225c80b39fcc938
Mostafizur <mostafizzfuad@gmail.com>
2025-09-14 22:23:30 +0600

    Fixed uploadthing

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { createUploadthing, type FileRouter } from "uploadthing/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { auth } from "@/auth";
 
