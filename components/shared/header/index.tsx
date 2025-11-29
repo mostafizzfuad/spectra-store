af96ded8370d6ebe1b98161b249607c53728cc28
Mostafizur <mostafizzfuad@gmail.com>
2025-11-30 02:10:21 +0600

    remove middlewire

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
+import Search from "./search";
 
 const Header = () => {
 	return (
 		<header className="w-full border-b">
 			<div className="wrapper flex-between">
 				<div className="flex-start">
+					<CategoryDrawer />
 					<Link href="/" className="flex-start ml-4">
 						<Image
 							src="/images/logo.svg"
@@ -22,6 +25,7 @@ const Header = () => {
 					</Link>
 				</div>
 				<div className="hidden md:block">
+					<Search />
 				</div>
 				<Menu />
 			</div>
